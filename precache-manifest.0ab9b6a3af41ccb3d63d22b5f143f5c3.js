b5a6b1a250cd2465b06e16d3841bdb91017abf3b
Ashwini <olixstudios@gmail.com>
2019-12-20 23:58:33 +0530

    react

---
+++ precache-manifest.0ab9b6a3af41ccb3d63d22b5f143f5c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967b604c62575f835430c9b6a470a752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e010c99d68c535879d45",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "13747041f0b9d76ba91a",
+    "url": "/static/js/2.1ba658b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1ba658b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e010c99d68c535879d45",
+    "url": "/static/js/main.95d4b92b.chunk.js"
+  },
+  {
+    "revision": "215b67208eb198e9fe36",
+    "url": "/static/js/runtime-main.9a605b21.js"
+  }
+]);
